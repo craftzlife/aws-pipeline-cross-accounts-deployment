1a97fb43bae0e94e2ee706428c2862c90ad1da61
Le Khai Hoan <khaihoan2711@gmail.com>
2025-03-15 15:58:31 +0700

    Add S3 Deployment

--- aws-cdk/lib/webapp-hosting-stack.ts
+++ aws-cdk/lib/webapp-hosting-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { AwsEnv } from '../bin/configs';
+import path = require('path');
 
 export class WebAppHostingStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -9,13 +10,32 @@ export class WebAppHostingStack extends cdk.Stack {
     const _HostingBucket = new cdk.aws_s3.Bucket(this, 'HostingBucket', {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
+      bucketKeyEnabled: false,
+      encryption: cdk.aws_s3.BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
+      blockPublicAccess: cdk.aws_s3.BlockPublicAccess.BLOCK_ALL,
     });
-    // const _S3Deployment = new cdk.aws_s3_deployment.BucketDeployment(this, 'S3Deployment', {
-    //   sources: [
-    //     cdk.aws_s3_deployment.Source.asset('../webapp/dist'),
-    //   ],
-    //   destinationBucket: _HostingBucket,
-    // });
+    
+    const _S3Deployment = new cdk.aws_s3_deployment.BucketDeployment(this, 'S3Deployment', {
+      sources: [
+        cdk.aws_s3_deployment.Source.asset(path.resolve(process.cwd(), '../angular-static-webapp'), {
+          bundling: {
+            image: cdk.DockerImage.fromRegistry('node:22'),
+            command: [
+              'sh', '-c', 
+              [
+                'npm i',
+                'npm run build',
+                'cp -r dist /asset-output'
+              ].join(' && ')
+            ],
+            user: 'root',
+          }
+        }),
+      ],
+      destinationBucket: _HostingBucket,
+      serverSideEncryptionAwsKmsKeyId: 'alias/aws/s3',
+    });
     
     // Temporary disable cloudfront as AWS require account to be verified before using
     // const _CloudFrontDistribution = new cdk.aws_cloudfront.Distribution(this, 'CloudfrontDistribution', {
